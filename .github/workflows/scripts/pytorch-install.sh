a8328c34fd5689adb6b336d66bd2953156cbafe8
Huiqiang Jiang <hjiang@microsoft.com>
2025-04-16 09:58:58 +0800

    Feature(MInference): update release cuda version (#134)
    Co-authored-by: Yucheng Li <liyucheng09@gmail.com>

--- .github/workflows/scripts/pytorch-install.sh
+++ .github/workflows/scripts/pytorch-install.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+# Copyright (c) 2024-2025 Microsoft
+# Licensed under The MIT License [see LICENSE for details]
+
 python_executable=python$1
 pytorch_version=$2
 cuda_version=$3
 
 pip install --upgrade pip
-# If we don't install before installing Pytorch, we get error for torch 2.0.1
-# ERROR: Could not find a version that satisfies the requirement setuptools>=40.8.0 (from versions: none)
-pip install lit
 # For some reason torch 2.2.0 on python 3.12 errors saying no setuptools
-pip install setuptools
+pip install setuptools==75.8.0
+# With python 3.13 and torch 2.5.1, unless we update typing-extensions, we get error
+# AttributeError: attribute '__default__' of 'typing.ParamSpec' objects is not writable
+pip install typing-extensions==4.12.2
 # We want to figure out the CUDA version to download pytorch
 # e.g. we can have system CUDA version being 11.7 but if torch==1.12 then we need to download the wheel from cu116
 # see https://github.com/pytorch/pytorch/blob/main/RELEASE.md#release-compatibility-matrix
@@ -17,8 +20,8 @@ pip install setuptools
 echo $MATRIX_CUDA_VERSION
 echo $MATRIX_TORCH_VERSION
 export TORCH_CUDA_VERSION=$(python -c "from os import environ as env; \
-minv = {'1.12': 113, '1.13': 116, '2.0': 117, '2.1': 118, '2.2': 118, '2.3': 118, '2.4': 118}[env['MATRIX_TORCH_VERSION']]; \
-maxv = {'1.12': 116, '1.13': 117, '2.0': 118, '2.1': 121, '2.2': 121, '2.3': 121, '2.4': 121}[env['MATRIX_TORCH_VERSION']]; \
+minv = {'2.2': 118, '2.3': 118, '2.4': 118, '2.5': 118, '2.6': 118}[env['MATRIX_TORCH_VERSION']]; \
+maxv = {'2.2': 121, '2.3': 121, '2.4': 124, '2.5': 124, '2.6': 124}[env['MATRIX_TORCH_VERSION']]; \
 print(max(min(int(env['MATRIX_CUDA_VERSION']), maxv), minv))" \
 )
 if [[ ${pytorch_version} == *"dev"* ]]; then
