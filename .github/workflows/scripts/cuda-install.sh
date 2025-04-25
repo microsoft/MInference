1b7f6ddb13132e35bf237692367d32fbcb3d4ca7
johnnynunez <johnnynuca14@gmail.com>
2025-04-25 18:45:31 +0200

    Feature(build): enhance CUDA architecture support and improve installation scripts

--- .github/workflows/scripts/cuda-install.sh
+++ .github/workflows/scripts/cuda-install.sh
@@ -5,8 +5,37 @@ cuda_version=$(echo $1 | tr "." "-")
 # Removes '-' and '.' ex: ubuntu-20.04 -> ubuntu2004
 OS=$(echo $2 | tr -d ".\-")
 
+ARCH=$(uname -m)
+ARCH_TYPE=$ARCH
+
+# Detectar si es Tegra
+if [[ "$ARCH" == "aarch64" ]]; then
+    if uname -a | grep -qi tegra; then
+        ARCH_TYPE="tegra-aarch64"
+    fi
+fi
+
+echo "Detected architecture: ${ARCH_TYPE}"
+
 # Installs CUDA
-wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-keyring_1.1-1_all.deb
+if [[ "$ARCH_TYPE" == "tegra-aarch64" ]]; then
+    # Jetson (Tegra)
+    wget -nv \
+        https://developer.download.nvidia.com/compute/cuda/repos/${OS}/arm64/cuda-${DISTRO}.pin \
+        -O /etc/apt/preferences.d/cuda-repository-pin-600
+
+elif [[ "$ARCH_TYPE" == "tegra-aarch64" ]]; then
+    # Jetson (Tegra)
+    wget -nv \
+        https://developer.download.nvidia.com/compute/cuda/repos/${OS}/arm64/cuda-${DISTRO}.pin \
+        -O /etc/apt/preferences.d/cuda-repository-pin-600
+else
+    # ARM64 SBSA (Grace)
+    wget -nv \
+        https://developer.download.nvidia.com/compute/cuda/repos/${OS}/sbsa/cuda-${DISTRO}.pin \
+        -O /etc/apt/preferences.d/cuda-repository-pin-600
+fi
+
 sudo dpkg -i cuda-keyring_1.1-1_all.deb
 rm cuda-keyring_1.1-1_all.deb
 sudo apt -qq update
